27b66d36f2b9fae9314f5d3815166fc685c14565
ramakala2022 <ramakala.ramakrishnan@tsworkz.com>
2022-03-28 19:43:38 +0530

    going to push employee python code

---
+++ employee/MainEmployee.py
@@ -0,0 +1,18 @@
+from EmployeeClass import *
+emp1 = Employee(1001,"Ram","Delhi",15000)
+emp2 = Employee(1002,"Joshi","Bangalore",20000)
+emp3 = Employee(1003,"Peter","Lucknow",25000)
+emp4 = Employee(1004,"Ashna","Chennai",20000)
+emp5 = Employee(1005,"Raghav","Kolkata",25000)
+
+
+print("------------------------------------------------------------------------")
+print("Employee id"+"\t"+"Employee location" +"\t" + "Employee name"+"\t"+"Employee salary")
+print("------------------------------------------------------------------------")
+
+
+emp1.GetEmployee()
+emp2.GetEmployee()
+emp3.GetEmployee()
+emp4.GetEmployee()
+emp5.GetEmployee()
